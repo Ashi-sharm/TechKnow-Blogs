d6d730fdf0604fa7099fc7255105d3078c271cc3
Ashi-sharm <sharmaashiis370@gmail.com>
2024-04-12 11:41:46 +0530

    Create the Footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <BrowserRouter>
@@ -26,6 +27,7 @@ export default function App() {
 
       
     </Routes>
+    <Footer/>
     </BrowserRouter>
 
   )
