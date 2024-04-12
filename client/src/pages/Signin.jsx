dfe626c2128ff0aa126cb37dd9d2fb948307f163
Ashi-sharm <sharmaashiis370@gmail.com>
2024-04-12 15:30:33 +0530

    complete signin page functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,118 @@
-import React from 'react'
 
-export default function Signin() {
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+
+
+export default function SignIn() {
+  const[formData, setFormData] = useState({});
+  const[errorMessage, setErrorMessage]= useState(null);
+  const[loading,setLoading]=useState(false);
+  const navigate = useNavigate();
+  const handleChange =(e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()});
+    
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.email || !formData.password) {
+      return setErrorMessage( 'please fill out all fields.');
+    }
+    
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const bodyReq = JSON.stringify(formData);
+      
+      const res = await fetch('/api/auth/signin',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: bodyReq,
+      }); 
+      
+      const data = await res.json(); 
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+
+      // setLoading(false);
+      if(res.ok){
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    
+      // setLoading(false);
+
+    }finally{
+   setLoading(false);
+
+    }
+
+  };
   return (
-    <div>Signin</div>
-  )
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto md:flex-row flex-col md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+          <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-pink-300 via-orange-300 to-yellow-300 rounded-lg text-white'> Ashi's</span>
+            Blog
+          </Link>
+          <p className='text-sm mt-5'> 
+            Transforming Your Thoughts into Words. you can sign in with your email and password or with Google 
+          </p>
+        </div>
+        
+        {/* right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='your email' />
+              <TextInput
+                type='email'
+                placeholder='user@gmail.com'
+                id='email' 
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value='your password' />
+              <TextInput
+                type='password'
+                placeholder='********'
+                id='password' 
+                onChange={handleChange}
+              />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                  loading ? (
+                  <>
+                  <Spinner size= 'sm'/>
+                  <span className='pl-3'>Loading....</span>
+                  </>
+                ) : ('Sign In')
+                }
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span> DontHave an account?</span>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign-In
+            </Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
+
