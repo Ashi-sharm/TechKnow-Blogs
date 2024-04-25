22c6c05d7835b3606d8ab4930c62d4c60c277bb0
Ashi-sharm <sharmaashiis370@gmail.com>
2024-04-25 13:00:16 +0530

    add and create a post page Ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -10,9 +10,10 @@ import { updateStart, updateFailure,updateSuccess } from '../redux/user/userSlic
 import { deleteUserStart, deleteUserFailure, deleteUserSuccess, signOutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle} from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-    const{currentUser, error} = useSelector((state) => state.user)
+    const{currentUser, error, loading} = useSelector((state) => state.user)
     const[imageFile, setImageFile] = useState(null);
     const[imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -198,9 +199,23 @@ export default function DashProfile() {
               defaultValue={currentUser.email} onChange={handleChange}/>
               <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
 
-              <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-                  Update
+              <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+                  {loading ? 'loading...': 'Update'}
               </Button>
+              {
+                currentUser.isAdmin &&(
+                  <Link to={'/create-post'}>
+                  <Button
+                  type='button'
+                  gradientDuoTone='purpleToPink'
+                  className='w-full'
+                  >
+                    Create a post
+                  </Button>
+                  </Link>
+                  
+                )
+              }
               <div className='text-red-500 flex justify-between mt-5'>
                   <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
                   <span onClick={handleSignOut} className='cursor-pointer'>Sign Out</span>
