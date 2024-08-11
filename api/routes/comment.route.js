2e940c6c70cab96bcf27f299ee6416c3a47bd359
Ashi-sharm <sharmaashiis370@gmail.com>
2024-08-11 18:34:28 +0530

    Add edit functionality to the comment component

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/comment.controller.js";
+import { createComment, editComment } from "../controllers/comment.controller.js";
 import { getPostComments} from "../controllers/comment.controller.js";
 import {likeComment} from '../controllers/comment.controller.js';
 
@@ -9,4 +9,5 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 export default router;
